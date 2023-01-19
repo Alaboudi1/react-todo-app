9ffec4597c9cf6373a6678d0f88d882ade872afd
Abdulaziz Alaboudi <abdulaziz@Abdulazizs-MacBook-Pro.local>
2023-01-19 16:24:28 -0500

    added bug

--- src/components/ui/InputBox.js
+++ src/components/ui/InputBox.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import enhance from '../hoc/wrapInputBox';
+import React from "react";
+import enhance from "../hoc/wrapInputBox";
 
-const InputBox = (props) =>{
-    const {addNew} = props;
-    const [inputs, updateInput] = React.useState('');
+const InputBox = (props) => {
+    const { addNew } = props;
+    const [inputs, updateInput] = React.useState("");
 
     const handleInputChanges = (e) => {
         updateInput(e.target.value);
-      };
+    };
 
-    const submit =(e) => {
-      addNew(inputs);
-      e.preventDefault();
-    }
+    const submit = (e) => {
+        addNew(inputs);
+        e.preventDefault();
+    };
+    const fetchData = () => {
+        fetch("https://www.boredapi.com/api/activity/").then((data) => {
+            if (data.activity) addNew(data.activity);
+        });
+    };
 
     return (
         <form onSubmit={submit}>
-        <input 
-            type="text"
-            className="form-control"
-            placeholder="Note"
-            onChange={handleInputChanges}
-        />
-              <input className="buttonAdd" type="submit" value="Add" />
-
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Note"
+                onChange={handleInputChanges}
+            />
+            <input className="buttonAdd" type="submit" value="Add" />
+            <input
+                className="buttonClear"
+                type="button"
+                value="Fetch"
+                onClick={fetchData}
+            />
         </form>
     );
-}
+};
 
 export default InputBox;
