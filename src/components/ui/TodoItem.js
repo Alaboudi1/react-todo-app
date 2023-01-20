6fc2e65ec46ada8558ee13ae0d505837f02011a7
Abdulaziz Alaboudi <abdulaziz@Abdulazizs-MacBook-Pro.local>
2023-01-20 09:58:16 -0500

    add bug

--- src/components/ui/TodoItem.js
+++ src/components/ui/TodoItem.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import CheckBox from './CheckBox';
+import React from "react";
+import CheckBox from "./CheckBox";
 
 export default function TodoItem(props) {
-    const {data, changeStatus} = props;
+    const { data, changeStatus } = props;
     const handleChange = (checked) => changeStatus(data.id, checked);
-    const className = 'todo-item ui-state-default ' + (data.completed === true ? 'completed' : 'pending');
+    const className =
+        "todo-item ui-state-default " +
+        (data.completed === true ? "completed" : "pending");
 
     return (
         <li className={className}>
             <div className="checkbox">
                 <label>
-                    <CheckBox checked={data.completed} onChange={handleChange}/> {data.text}
+                    <CheckBox
+                        checked={data.completed}
+                        onChange={handleChange}
+                    />{" "}
+                    {data.text}
                 </label>
+                <button
+                    className="btn btn-default btn-xs pull-right"
+                    onClick={() => (data.completed = !data.completed)}
+                >
+                    Check
+                </button>
             </div>
         </li>
     );
