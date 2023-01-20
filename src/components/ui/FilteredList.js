0563ddd7ae2986395dea5cd8f413fefe45de3a11
Abdulaziz Alaboudi <abdulaziz@Abdulazizs-MacBook-Pro.local>
2023-01-19 21:46:32 -0500

    added bug

--- src/components/ui/FilteredList.js
+++ src/components/ui/FilteredList.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import TodoItem from './TodoItem';
-import {MSG_NO_ITEMS} from '../../assets/text/en_US';
+import React from "react";
+import { MSG_NO_ITEMS } from "../../assets/text/en_US";
+import CheckBox from "./CheckBox";
 
 export default function FilteredList(props) {
-    const {items, changeStatus} = props;
+    const { items, changeStatus } = props;
+    const handleChange = (id, checked) => changeStatus(id, checked);
+    const handleRemove = (id) => console.log("remove", id);
+    const className = (item) =>
+        "todo-item ui-state-default " +
+        (item.completed === true ? "completed" : "pending");
 
     if (items.length === 0) {
-        return (
-            <p className="alert alert-info">{MSG_NO_ITEMS}</p>
-        );
+        return <p className="alert alert-info">{MSG_NO_ITEMS}</p>;
     }
 
     return (
         <ul className="list-unstyled">
-            {items.map(item => (
-                <TodoItem key={item.id} data={item} changeStatus={changeStatus}/>
+            {items.map((item) => (
+                <li className={className(item)} key={item.id}>
+                    <div className="checkbox">
+                        <label>
+                            <CheckBox
+                                checked={item.completed}
+                                onChange={(checked) =>
+                                    handleChange(item.id, checked)
+                                }
+                            />{" "}
+                            {item.text}
+                        </label>
+                    </div>
+                    <button
+                        onClick={() => handleRemove(item.id)}
+                        className="btn btn-danger btn-xs"
+                        style={{
+                            marginLeft: "10px",
+                            marginTop: "5px",
+                            float: "right",
+                        }}
+                    >
+                        X
+                    </button>
+                </li>
             ))}
         </ul>
     );
