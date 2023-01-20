54c8890575494732a4ad95d1af69b49401b9860d
Abdulaziz Alaboudi <abdulaziz@Abdulazizs-MacBook-Pro.local>
2023-01-20 10:10:36 -0500

    added bug

--- src/components/ui/InputBox.js
+++ src/components/ui/InputBox.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import enhance from '../hoc/wrapInputBox';
+import React from "react";
+import enhance from "../hoc/wrapInputBox";
 
-const InputBox = (props) =>{
-    const {addNew} = props;
-    const [inputs, updateInput] = React.useState('');
+const InputBox = (props) => {
+    const { addNew } = props;
+    const [inputs, updateInput] = React.useState("");
 
     const handleInputChanges = (e) => {
         updateInput(e.target.value);
-      };
+    };
 
-    const submit =(e) => {
-      addNew(inputs);
-      e.preventDefault();
-    }
+    const submit = (e) => {
+        addNew(inputs);
+        e.preventDefault();
+    };
 
     return (
-        <form onSubmit={submit}>
-        <input 
-            type="text"
-            className="form-control"
-            placeholder="Note"
-            onChange={handleInputChanges}
-        />
-              <input className="buttonAdd" type="submit" value="Add" />
-
-        </form>
+        <>
+            <form onSubmit={(e) => submit(e)}>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Note"
+                    onChange={handleInputChanges}
+                    value={inputs}
+                />
+                <input className="buttonAdd" type="submit" value="Add" />
+                <button className="buttonClear" onClick={() => updateInput("")}>
+                    Clear
+                </button>
+            </form>
+        </>
     );
-}
+};
 
 export default InputBox;
