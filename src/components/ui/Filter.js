7032efe8c0344bd9d540e4618725e067ba7c6208
Abdulaziz Alaboudi <abdulaziz@Abdulazizs-MacBook-Pro.local>
2023-01-23 13:17:52 -0500

    added bug

--- src/components/ui/Filter.js
+++ src/components/ui/Filter.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import {getOptions} from '../../services/filter';
+import React from "react";
+import { getOptions } from "../../services/filter";
 
 export default function Filter(props) {
     const options = getOptions();
-    const {filter, changeFilter} = props;
-    const getClass = (key) => (key === filter ? 'selected' : '');
+    const { filter, changeFilter } = props;
+    const getClass = (key) => (key === filter ? "selected" : "");
 
     return (
         <ul className="filters list-unstyled clearfix">
-            {Object.keys(options).map(key => (
-                <li key={key}>
-                    <a onClick={() => changeFilter(key)} className={getClass(key)}>
+            <label>
+                {Object.keys(options).map((key) => (
+                    <li key={key} style={{ marginRight: "5px" }}>
+                        <input
+                            type="radio"
+                            onClick={() => changeFilter(key)}
+                            className={getClass(key)}
+                            name="filter"
+                        />
                         {options[key]}
-                    </a>
-                </li>
-            ))}
+                    </li>
+                ))}
+            </label>
         </ul>
     );
 }
