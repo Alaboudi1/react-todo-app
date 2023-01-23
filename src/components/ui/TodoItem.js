22573a7418538d9669049c4afdb655b22e0ac98f
Abdulaziz Alaboudi <abdulaziz@Abdulazizs-MacBook-Pro.local>
2023-01-23 11:43:09 -0500

    bug added

--- src/components/ui/TodoItem.js
+++ src/components/ui/TodoItem.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import CheckBox from './CheckBox';
+import React from "react";
+import CheckBox from "./CheckBox";
 
 export default function TodoItem(props) {
-    const {data, changeStatus} = props;
+    const { data, changeStatus } = props;
     const handleChange = (checked) => changeStatus(data.id, checked);
-    const className = 'todo-item ui-state-default ' + (data.completed === true ? 'completed' : 'pending');
+    const itemRef = React.useRef(null);
+
+    const className =
+        "todo-item ui-state-default " +
+        (data.completed === true ? "completed" : "pending");
+
+    React.useEffect(() => {
+        // setTimeout(() => {
+        itemRef.current.style.transform = "translatex(25px)";
+        itemRef.current.style.opacity = 1;
+        itemRef.current.style.transition = "all 0.5s ease-in-out";
+        // }, 100);
+    }, [data]);
 
     return (
-        <li className={className}>
+        <li className={`${className}`} ref={itemRef}>
             <div className="checkbox">
                 <label>
-                    <CheckBox checked={data.completed} onChange={handleChange}/> {data.text}
+                    <CheckBox
+                        checked={data.completed}
+                        onChange={handleChange}
+                    />{" "}
+                    {data.text}
                 </label>
             </div>
         </li>
